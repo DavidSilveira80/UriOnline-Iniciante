05a7262741ce6a5184d2c443184268bf583cf511
DavidSilveira80 <davidpscontato@gmail.com>
2023-04-13 16:28:03 -0300

    Adicionado enunciado e separada por função.

--- Ex1154.py
+++ Ex1154.py
@@ -1,14 +1,44 @@
-individuos = soma_idades = media_idades = 0
+"""
+Faça um algoritmo para ler um número indeterminado de dados, contendo cada um,
+a idade de um indivíduo. O último dado, que não entrará nos cálculos, contém o
+valor de idade negativa. Calcular e imprimir a idade média deste grupo de indivíduos.
+
+Entrada
+A entrada contém um número indeterminado de inteiros. A entrada será encerrada quando
+um valor negativo for lido.
+
+Saída
+A saída contém um valor correspondente à média de idade dos indivíduos.
+
+A média deve ser impressa com dois dígitos após o ponto decimal.
+
+Exemplo de Entrada	         Exemplo de Saída
+
+34                           39.25
+56
+44
+23
+-2
+
+"""
+
+
+soma_idades_individuos = []
+media_idades = qtd_individuos = 0
+
+
+def calcula_media_das_idades(soma_idades, numero_individuos):
+    return sum(soma_idades) / numero_individuos
+
 
 while True:
+    idades_individuos = int(input())
 
-    idade = int(input())
-
-    if idade < 0:
+    if idades_individuos < 0:
         break
     else:
-        soma_idades += idade
-        individuos += 1
+        soma_idades_individuos.append(idades_individuos)
+        qtd_individuos += 1
 
-media_idades = soma_idades / individuos
+media_idades = calcula_media_das_idades(soma_idades_individuos, qtd_individuos)
 print(f'{media_idades:.2f}')
