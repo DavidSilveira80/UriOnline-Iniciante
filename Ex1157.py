17ef7185741b1a534f78c566fc8fb71c169cb066
DavidSilveira80 <davidpscontato@gmail.com>
2023-04-13 16:00:37 -0300

    Adicionado enunciado e separado por função.

--- Ex1157.py
+++ Ex1157.py
@@ -1,6 +1,30 @@
-X = int(input())
+"""
+Ler um número inteiro N e calcular todos os seus divisores.
 
-for divisor in range(1, X + 1):
-    if X % divisor == 0:
-        print(divisor)
-        
+Entrada
+O arquivo de entrada contém um valor inteiro.
+
+Saída
+Escreva todos os divisores positivos de N, um valor por linha.
+
+Exemplo de Entrada	  Exemplo de Saída
+
+6                     1
+                      2
+                      3
+                      6
+"""
+
+
+def mostra_divisores(dividendo, divisor):
+    if dividendo % divisor == 0:
+        return divisor
+
+
+dividendo = int(input())
+
+for divisor in range(1, dividendo + 1):
+    if mostra_divisores(dividendo, divisor) == None:
+        continue
+    else:
+        print(mostra_divisores(dividendo, divisor))
