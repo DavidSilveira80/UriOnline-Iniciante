93a25433305d4c8cab7a8eaf9381071d1e142b0a
DavidSilveira80 <davidpscontato@gmail.com>
2023-04-13 19:02:19 -0300

    Adicionado enunciado e dividido por funções.

--- Ex1117.py
+++ Ex1117.py
@@ -1,17 +1,52 @@
-notas_validas = soma = media = 0
+"""
+Faça um programa que leia as notas referentes às duas avaliações de um aluno.
+Calcule e imprima a média semestral. Faça com que o algoritmo só aceite notas
+válidas (uma nota válida deve pertencer ao intervalo [0,10]). Cada nota deve ser validada separadamente.
+
+Entrada
+A entrada contém vários valores reais, positivos ou negativos. O programa deve ser
+encerrado quando forem lidas duas notas válidas.
+
+Saída
+Se uma nota inválida  for lida, deve ser impressa a mensagem "nota invalida".
+Quando duas notas válidas forem lidas, deve ser impressa a mensagem
+"media = " seguido do valor do cálculo. O valor deve ser apresentado com duas casas após o ponto decimal.
+
+Exemplo de Entrada	   Exemplo de Saída
+
+-3.5                   nota invalida
+3.5                    nota invalida
+11.0                   media = 6.75
+10.0
+
+"""
+
+
+def validar_notas(nota):
+    if 0 <= nota <= 10:
+        valida = True
+    else:
+        valida = False
+    return valida
+
+
+def calcular_media(notas_somadas):
+    return notas_somadas / 2
+
+
+notas_validas = soma_das_notas = 0
 
 while True:
 
     notas = float(input())
 
-    if notas >= 0 and notas <= 10:
+    if validar_notas(notas):
         notas_validas += 1
-        soma += notas
+        soma_das_notas += notas
         if notas_validas == 2:
             break
-    elif notas < 0 or notas > 10:
+    else:
         print('nota invalida')
         continue
 
-media = soma / 2
-print(f'media = {media:.2f}')
+print(f'media = {calcular_media(soma_das_notas):.2f}')
