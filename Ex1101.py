457f0ad313e16d3127c2ed2b4ff42c037d1f8a16
DavidSilveira80 <davidpscontato@gmail.com>
2023-04-14 00:01:46 -0300

    Sequencia de Números e Soma, adicionado enunciado e dividido com funções.

--- Ex1101.py
+++ Ex1101.py
@@ -1,26 +1,59 @@
-from telnetlib import STATUS
+"""
+Leia um conjunto não determinado de pares de valores
+M e N (parar quando algum dos valores for menor ou igual a zero).
+Para cada par lido, mostre a sequência do menor até o maior e a
+soma dos inteiros consecutivos entre eles (incluindo o N e M).
+
+Entrada
+O arquivo de entrada contém um número não determinado de valores M e N.
+A última linha de entrada vai conter um número nulo ou negativo.
+
+Saída
+Para cada dupla de valores, imprima a sequência do menor até o maior e a soma deles, conforme exemplo abaixo.
+
+Exemplo de Entrada	   Exemplo de Saída
+
+5 2                    2 3 4 5 Sum=14
+6 3                    3 4 5 6 Sum=18
+5 0
+
+"""
+
+
+def informando_valores():
+    valores = input().split(' ')
+    return int(valores[0]), int(valores[1])
+
+
+def gera_sequencia(N, M):
+    saida = []
+    if N > M:
+        for numero in range(M, N + 1):
+            saida.append(numero)
+    else:
+        for numero in range(N, M + 1):
+            saida.append(numero)
+    return saida
+
+
+def converter_sequencia_para_string(sequencia_inteira):
+    sequencia_string = []
+    for inteiro in sequencia_inteira:
+        sequencia_string.append(str(inteiro))
+    return sequencia_string
+
+
+def gera_saida(sequencia_string, sequencia_inteira):
+    seq = ' '.join(sequencia_string)
+    return seq + f' Sum={sum(sequencia_inteira)}'
 
 
 while True:
-    valor1, valor2 = input().split()
-    N = int(valor1)
-    M = int(valor2)
+    N, M = informando_valores()
     soma = 0
     if N <= 0 or M <= 0:
         break
-    elif N > M:
-
-        for numero in range(M, N + 1):
-            soma += numero
-            if numero != N:
-                print(f'{numero} ',end='')
-            else:    
-                print(f'{numero} ' + f'{soma}\n', end='')
-    elif N < M:
-         
-         for numero in range(N, M + 1):
-             soma += numero
-             if numero != M:
-                 print(f'{numero} ',end='')
-             else:
-                 print(f'{numero} ' + f'Sum={soma}\n',end='')
+    else:
+        sequencia_inteira = gera_sequencia(N, M)
+        sequencia_string = converter_sequencia_para_string(sequencia_inteira)
+        print(gera_saida(sequencia_string, sequencia_inteira))
