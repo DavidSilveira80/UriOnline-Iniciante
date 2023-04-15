6d74cc8169013be974ae35118317d6013b1b8732
DavidSilveira80 <davidpscontato@gmail.com>
2023-04-15 16:03:52 -0300

    Maior e Posição.

---
+++ Ex1080.py
@@ -0,0 +1,40 @@
+"""
+Leia 100 valores inteiros. Apresente então o maior valor lido e a posição dentre os 100 valores lidos.
+
+Entrada
+O arquivo de entrada contém 100 números inteiros, positivos e distintos.
+
+Saída
+Apresente o maior valor lido e a posição de entrada, conforme exemplo abaixo.
+
+Exemplo de Entrada	      Exemplo de Saída
+
+2                          34565
+113                        4
+45
+34565
+6
+...
+8
+
+"""
+
+
+def retorna_o_maior_numero(numeros):
+    return max(numeros)
+
+
+def retorna_posicao_do_maior_numero(maior_numero, numeros):
+    return numeros.index(maior_numero) + 1
+
+
+numeros = []
+for ciclo in range(1, 101):
+    numero = int(input())
+    numeros.append(numero)
+
+
+maior_numero = retorna_o_maior_numero(numeros)
+posicao_do_maior_numero = retorna_posicao_do_maior_numero(maior_numero, numeros)
+
+print(f'{maior_numero}\n{posicao_do_maior_numero}')
