d1eff538922e58c9a6d3f5c553d0c34fab49477c
DavidSilveira80 <davidpscontato@gmail.com>
2023-04-17 17:03:20 -0300

    Papagaio Poliglota

---
+++ Ex2850.py
@@ -0,0 +1,42 @@
+"""
+Humberto tem um papagaio muito esperto. Quando está com as duas pernas no chão, o papagaio fala em
+português. Quando levanta a perna esquerda, fala em inglês. Por fim, quando levanta a direita fala
+em francês. Nico, amigo de Humberto, ficou fascinado com o animal. Em sua emoção perguntou:
+“E quando ele levanta as duas?”. Antes que Humberto pudesse responder, o papagaio gritou: “Ai eu caio, idiota!”.
+
+Entrada
+A entrada consiste de diversos casos de teste. Cada caso de teste consiste uma string informando qual a
+situação de levantamento de pernas do papagaio.
+
+Saída
+Para cada condição de levantamento de pernas do papagaio, imprima a linguagem que ele utilizará.
+Caso ele levante as duas pernas, imprima “caiu”. Quebre uma linha a cada caso de teste.
+
+Exemplo de Entrada	    Exemplo de Saída
+
+esquerda                 ingles
+direita                  frances
+nenhuma                  portugues
+as duas                  caiu
+"""
+
+
+def mostra_idioma(patas_levantadas):
+    if patas_levantadas == 'esquerda':
+        idioma = 'ingles'
+    elif patas_levantadas == 'direita':
+        idioma = 'frances'
+    elif patas_levantadas == 'nenhuma':
+        idioma = 'portugues'
+    elif patas_levantadas == 'as duas':
+        idioma = 'caiu'
+
+    return idioma
+
+
+while True:
+    try:
+        patas_levantadas = input()
+        print(mostra_idioma(patas_levantadas))
+    except EOFError:
+        break
